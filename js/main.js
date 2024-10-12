39c47a056cf81a0fb1a72723307dec19db33dd30
HollySloat <157514488+DollyHalo@users.noreply.github.com>
2024-10-11 22:30:05 -0500

    added starter files

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// JS scripts placed here
+
+
+// function handleClick(ev) {
+//     // Prevent the default action of the event
+//     ev.preventDefault();
+
+//     // declare variables for use
+//     const body = document.querySelector('body');
+
+//     // Get the value of the input field
+//     const nameInput = document.getElementById('name');
+//     const nameValue = nameInput.value;
+//     console.log('nameValue:', nameValue);
+//     alert('You clicked me!, ' + nameValue);
+
+//     // Perform action: add class to body element
+//     body.classList.add('form-submitted');
+// }
+
+const button = document.getElementById('submit');
+
+button.addEventListener("click", (ev) => {
+    // Prevent the default action of the event
+    ev.preventDefault();
+
+    // declare variables for use
+    const body = document.querySelector('body');
+    const nameOutput = document.getElementById('form-name');
+    console.log(nameOutput);
+
+
+    // Get the value of the input field
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    console.log('nameInput:', nameInput);
+    console.log('nameValue:', nameValue);
+    nameOutput.innerHTML = nameValue;
+    // alert('You clicked me!, ' + nameValue);
+
+    // Perform action: add class to body element
+    body.classList.add('form-submitted');
+
+    window.scrollTo(0, 0);
+});
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
